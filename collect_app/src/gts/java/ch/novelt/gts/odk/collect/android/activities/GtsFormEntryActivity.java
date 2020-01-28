cb4ae46adbae2ed9203e6dd09b334e943ead4f34
ybenkhaled <yassine.benkhaled@gmail.com>
2020-01-28 19:55:59 +0100

    fix last instance URI not provided due to the fix (intent action not EDIT) to avoid conflict between GTS Collect & ODK Collect

--- collect_app/src/gts/java/ch/novelt/gts/odk/collect/android/activities/GtsFormEntryActivity.java
+++ collect_app/src/gts/java/ch/novelt/gts/odk/collect/android/activities/GtsFormEntryActivity.java
@@ -1,8 +1,17 @@
 package ch.novelt.gts.odk.collect.android.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import org.odk.collect.android.activities.FormEntryActivity;
 
 public class GtsFormEntryActivity extends FormEntryActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getIntent().setAction(Intent.ACTION_EDIT);
+    }
 
     @Override
     protected void createQuitDialog() {
