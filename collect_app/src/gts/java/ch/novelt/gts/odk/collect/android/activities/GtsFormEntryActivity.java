f25bc074f4b15d1ab01c5efcada312512ddc5a23
ybenkhaled <yassine.benkhaled@gmail.com>
2020-02-19 14:26:12 +0100

    Remove menu icons when filling an ODK Form

--- collect_app/src/gts/java/ch/novelt/gts/odk/collect/android/activities/GtsFormEntryActivity.java
+++ collect_app/src/gts/java/ch/novelt/gts/odk/collect/android/activities/GtsFormEntryActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import org.odk.collect.android.activities.FormEntryActivity;
+import org.odk.collect.android.preferences.AdminKeys;
+import org.odk.collect.android.preferences.AdminSharedPreferences;
 
 public class GtsFormEntryActivity extends FormEntryActivity {
 
@@ -11,6 +13,9 @@ public class GtsFormEntryActivity extends FormEntryActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getIntent().setAction(Intent.ACTION_EDIT);
+        AdminSharedPreferences.getInstance().save(AdminKeys.KEY_SAVE_MID, false);
+        AdminSharedPreferences.getInstance().save(AdminKeys.KEY_ACCESS_SETTINGS, false);
+        AdminSharedPreferences.getInstance().save(AdminKeys.KEY_MARK_AS_FINALIZED, false);
     }
 
     @Override
